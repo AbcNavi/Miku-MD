4d853309fc6101adeb5a6404047f5d0954ddd1f9
AbcNavi <118669749+AbcNavi@users.noreply.github.com>
2022-12-09 21:26:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
+global.Owner = ['94769602486']
+global.OwnerNumber = ['94769602486']
+global.ownertag = ['94769602486']
+global.BotName = "Queen Baby"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.OwnerName = "NaviRox"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
